3e5c196f6ad6f0ab2d88745eac6d2e1802f3059c
aevlp <22375@qq.com>
2019-12-30 13:55:57 +0800

    test

---
+++ easymall-config-server/src/test/java/cn/cy/config/EasymallConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.cy.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasymallConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
